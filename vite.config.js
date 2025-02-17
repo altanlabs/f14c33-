ae7644b53b45bdd6aca664d7fc8262aabe50d65e
Altan <albert@altan.ai>
2025-02-17 05:59:04 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+
+// DO NOT CHANGE '@vitejs/plugin-react-swc' if not the previews will stop working. 
+
+import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    rollupOptions: {
+      onwarn: function (warning, warn) {
+        if (warning.code === "ESLINT_ERROR") return;
+        warn(warning);
+      },
+    },
+  },
+});
